1b17df0d0700839d5e0639d3e2f2a4b8d1bdc878
DevKoenv <koenvanvlimmeren@gmail.com>
2024-09-04 13:27:52 +0200

    Initial commit

---
+++ core/src/main/kotlin/dev/koenv/fantasy/Main.kt
@@ -0,0 +1,6 @@
+package dev.koenv.fantasy
+
+import com.badlogic.gdx.ApplicationAdapter
+
+/** [com.badlogic.gdx.ApplicationListener] implementation shared by all platforms. */
+class Main : ApplicationAdapter()
